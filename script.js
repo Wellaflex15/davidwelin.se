35a46bbe048de8d236468f449557a175f2cba656
David Welin <david.welin@outlook.com>
2021-02-24 18:28:26 +0100

    Div ändringar

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let oscar = document.getElementById('main_picture');
-let klicker = document.getElementById('clicks');
+// Canvas code
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
 
-oscar.addEventListener('click', function() {
-    let header = document.getElementById('main-header');
-    header.classList.toggle('hidden');
-    klicker.innerHTML++;
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 150, 75);
 
-});
+// Logo code
+async function animate(e){
+    const el = e.currentTarget;
+    
+    el.firstChild.textContent = 'Utvecklare';
+}
+
+const logo = document.querySelector('.logo');
+console.log(logo);
+logo.addEventListener('click', animate);
 
 
